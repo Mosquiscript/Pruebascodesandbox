4520f41104edb314eefddbbb945da6938393cfe1
Teamhorda <56400522+Teamhorda@users.noreply.github.com>
2020-11-01 22:27:26 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/* import "./styles.css";
+
+document.getElementById("navbar").innerHTML = `
+<nav class="navbar">
+<div class="container navbar_content">
+  <div>
+    <i class="fas fa-mobile-alt"></i>
+    Recargas
+  </div>
+  <div class="my-1 btn_apartados">
+    <a href="src/objetos_recargas/objetos_recargas.html"> apartado 1</a>
+  </div>
+  <div class="my-1 btn_apartados">apartado 2</div>
+  <div class="my-1 btn_apartados">apartado 3</div>
+  <button class="btn_dark">
+    <i class="far fa-moon"></i>
+    Dark mode
+  </button>
+</div>
+</nav>
+`; */
