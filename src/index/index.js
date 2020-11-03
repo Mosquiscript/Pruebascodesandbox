56c6eade23fc6a597fb490f1156c007312ce802d
Mosqui <56400522+Mosquiscript@users.noreply.github.com>
2020-11-02 22:52:46 -0600

    nuevos metodos metodo click

--- src/index/index.js
+++ src/index/index.js
@@ -1,21 +1,5 @@
-/* import "./styles.css";
+const button_mode = document.getElementById("button_mode");
 
-document.getElementById("navbar").innerHTML = `
-<nav class="navbar">
-<div class="container navbar_content">
-  <div>
-    <i class="fas fa-mobile-alt"></i>
-    Recargas
-  </div>
-  <div class="my-1 btn_apartados">
-    <a href="src/objetos_recargas/objetos_recargas.html"> apartado 1</a>
-  </div>
-  <div class="my-1 btn_apartados">apartado 2</div>
-  <div class="my-1 btn_apartados">apartado 3</div>
-  <button class="btn_dark">
-    <i class="far fa-moon"></i>
-    Dark mode
-  </button>
-</div>
-</nav>
-`; */
+button_mode.addEventListener("click", () => {
+  alert("hola");
+});
